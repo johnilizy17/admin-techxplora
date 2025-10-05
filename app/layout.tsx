9e5866b477786ab0eb20731c067e6355985460dc
John Abraham <abrahamtosinj@gmail.com>
2025-10-05 10:47:40 +0100

    meta design

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ export const metadata: Metadata = {
   title: "Techxplora Dashboard",
   description:
     "A fully responsive analytics dashboard featuring dynamic charts, interactive tables, a collapsible sidebar, and a light/dark mode theme switcher. Built with modern web technologies, it ensures seamless performance across devices, offering an intuitive user interface for data visualization and exploration.",
+    icons:"/logo.png"
 };
 
 export default async function RootLayout({
